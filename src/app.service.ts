b61d94be7f71764317a656229a2aaa4ee902b5c8
prasharma0 <prashant.sharma.ps725@gmail.com>
2022-05-19 15:46:08 +0545

    SKT-5:Configures to use environment variable using the .env file

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string{
+    console.log(process.env.DATABASE_NAME);
+    console.log(process.env.SECRET_KEY);
     return 'Hello Docker!';
   }
 }
